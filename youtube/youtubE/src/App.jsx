10550c5f2a47cb78b6447718e4231ac5d5ac5f69
martinristow <martinristov084@gmail.com>
2025-01-22 00:33:01 +0100

    Added videos to homepage

--- youtube/youtubE/src/App.jsx
+++ youtube/youtubE/src/App.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const VIDEOS = [
+    {
+        id: 1,
+        title: "How to learn React",
+        url: "https://www.youtube.com/watch?v=SqcY0GlETPk&t=163s",
+        cover: "https://i.ytimg.com/vi/SqcY0GlETPk/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBvF7R7tYEZqgAYn6fM5A_QgI1e-A"
+    },
+    {
+        id: 2,
+        title: "How to learn CSS",
+        url: "https://www.youtube.com/watch?v=Z4pCqK-V_Wo",
+        cover: "https://i.ytimg.com/vi/Z4pCqK-V_Wo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBPA3sbcpqOW9aUTVIWfzwVxlLQnQ"
+    },
+];
 
-  return (
-    <>
-     <p>Zdravo</p>
-    </>
-  )
+
+const App = () => {
+    return (
+        <>
+            { VIDEOS.map( video => {
+                return <div><a href={video.url} target='_blank'>
+                <h3>{video.title}</h3>
+                <img src={video.cover}></img>
+                    </a>
+                </div>
+            }) }
+        </>
+    )
 }
+
 
 export default App
