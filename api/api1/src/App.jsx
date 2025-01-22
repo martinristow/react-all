9188851ce2f9d00d891ae899bea59616e9aa8887
martinristow <martinristov084@gmail.com>
2025-01-22 13:46:21 +0100

    Added Axios to get movie by title

--- api/api1/src/App.jsx
+++ api/api1/src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css'
+import axios from "axios";
 
 
-console.log(import.meta.env.VITE_OMDBAPI_KEY)
+// console.log(import.meta.env.VITE_OMDBAPI_KEY)
+// console.log(import.meta.env.VITE_APP_OMDBAPI_URL)
+
+// fetch("")
+//     .then(response => response.json()) // pretvarame go odgovorot u JSON format
+//     .then(jsonResponse => console.log(jsonResponse));
+
+axios.get(import.meta.env.VITE_APP_OMDBAPI_URL+"?t=Terminator&apikey="+import.meta.env.VITE_OMDBAPI_KEY)
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+
 
 function App() {
 
