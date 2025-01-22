129f6ee8c91031874c73fcb09a3b0bc13816f651
martinristow <martinristov084@gmail.com>
2025-01-22 14:13:48 +0100

    Separating logic into components

--- api/api1/src/App.jsx
+++ api/api1/src/App.jsx
@@ -4,9 +4,7 @@ import Movies from "./Components/Movies.jsx";
 
 
 
-axios.get(import.meta.env.VITE_APP_OMDBAPI_URL+"?t=Terminator&apikey="+import.meta.env.VITE_OMDBAPI_KEY)
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
+
 
 
 function App() {
--- api/api1/src/Components/Movies.jsx
+++ api/api1/src/Components/Movies.jsx
@@ -1,13 +1,27 @@
+import {useState} from "react";
+import axios from "axios";
 
 
 function Movies() {
 
+    const [movieName, setMovieName] = useState();
 
+    const searchMovieByTitle = () => {
+
+        const OMDBAPI_URL = import.meta.env.VITE_APP_OMDBAPI_URL
+        const OMDBAPI_KEY = import.meta.env.VITE_OMDBAPI_KEY
+
+        axios.get(`${OMDBAPI_URL}?t=${movieName}&apikey=${OMDBAPI_KEY}`)
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error))
+    }
 
     return (
         <>
-            <input type="text" placeholder="Vnesi nekoj film"/>
-            <button onClick={}>Prebaraj</button>
+            <form action="">
+                <input onInput={e => setMovieName(e.target.value)} type="text" placeholder="Vnesi nekoj film"/>
+                <button  onClick={searchMovieByTitle} type="button">Search Movie</button>
+            </form>
         </>
     );
 }
