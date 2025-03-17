ee602c1c01fe239a12765023edc3e63e6c8013d9
martinristow <martinristov084@gmail.com>
2025-03-17 13:07:03 +0100

    Add Product Component and connect them with App.jsx

---
+++ roadmap/Components/Components-React/src/Components/Product.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../style.css'
+
+const products = {
+    "Samsung A55": 31000,
+    "Samsung A70": 24000,
+    "Iphone 15 Pro": 65000,
+    "Samsung Galaxy 21": 55000
+};
+
+const Product = (props) => {
+    return (
+        <div>
+
+        {
+            Object.entries(products).map(([phone, price]) => (
+                <p>{phone}, price: {price} MKD, with tax: {calculateTax(price, props.tax)}</p>
+            ))
+
+        }
+
+        </div>
+    )
+}
+
+
+function calculateTax(price, tax) {
+    return ((price * tax) / 100) + price
+}
+
+export default Product
