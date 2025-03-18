82feed47cec5cd8527fa11530423988cedadef03
martinristow <martinristov084@gmail.com>
2025-03-18 20:12:49 +0100

    Added a new folder Pages and loading AllVideos component

---
+++ roadmap/Search/youtube/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AllVideos from "../Components/AllVideos.jsx";
+
+const Home = () => {
+    return (
+        <>
+        <AllVideos/>
+        </>
+    )
+}
+export default Home
