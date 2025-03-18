8ae61d35eb3d8a8ad131f04b984c309e4b4faec6
martinristow <martinristov084@gmail.com>
2025-03-18 20:13:54 +0100

    Separated this code on different files to have a better structure for our project and implement react-route to have routing system

--- roadmap/Search/youtube/src/App.jsx
+++ roadmap/Search/youtube/src/App.jsx
@@ -1,32 +1,25 @@
 import React from 'react'
+import AllVideos from "./Components/AllVideos.jsx";
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./Pages/Home.jsx";
+import Video from "./Components/Video.jsx";
 
-const VIDEOS = [
-    {
-        id: 1,
-        title: "How to Remember Everything You Read",
-        url: "https://www.youtube.com/watch?v=okHkUIW46ks&t=64s",
-        cover: "https://i.ytimg.com/vi/okHkUIW46ks/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAfA05NGmNvSuKpRtpB-8Li0-eHYQ"
-    },
-    {
-        id: 2,
-        title: "Jordan Peterson - How to Remember Everything You Read",
-        url: "https://www.youtube.com/watch?v=okHkUIW46ks&t=64s",
-        cover: "https://i.ytimg.com/vi/ch55SHnLs_M/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBhRFIUmhhSNKYo64N2PDQLuu1qLA"
-    }
-];
+// path = http://localhost:5173/
+// path "/" => http://localhost:5173/
+// path "/about" => http://localhost:5173/about
+// /video/:id => id, 1, 5, 7, 10, 12 => wildcard
+// /video/12
 
 const App = () => {
     return (
         <>
-            {
-                VIDEOS.map((video, index) => {
-                    return <a key={index} href={video.url} target="_blank">
-                        <img src={video.cover} alt="cover"/>
-                        <h3>{video.title}</h3>
-                    </a>
-                })
-            }
-
+            {/*<AllVideos/>*/}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/video/:id" element={<Video/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
