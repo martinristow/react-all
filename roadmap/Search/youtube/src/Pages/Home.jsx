520c113563d57777f834cfbf168007b5648e99c2
martinristow <martinristov084@gmail.com>
2025-03-19 00:07:33 +0100

    Enhance Home component with search functionality
    Added useState to manage videos and filtered videos.
    Imported VIDEOS from videos.json for initial state.
    Integrated SearchBar component to allow video filtering.
    Implemented handleSearchVideo function to update filteredVideos based on search results.
    Updated AllVideos component to display filtered videos dynamically.

--- roadmap/Search/youtube/src/Pages/Home.jsx
+++ roadmap/Search/youtube/src/Pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AllVideos from "../Components/AllVideos.jsx";
+import SearchBar from "../Components/SearchBar.jsx";
+import VIDEOS from '../videos.json'
 
 const Home = () => {
+    let [videos, setVideos] = useState(VIDEOS); // THIS WILL SHOW ALL VIDEOS
+    let [filteredVideos, setFilteredVideos] = useState(VIDEOS); // THIS IS THE PART THAT IS CHANGING
+
+    const handleSearchVideo = (results) => {
+        setFilteredVideos(results);
+    }
+
+    console.log(filteredVideos);
+
     return (
         <>
-        <AllVideos/>
+            <SearchBar videos={videos} onSearchVideo={handleSearchVideo}/>
+            <AllVideos videos={filteredVideos}/>
         </>
     )
 }
