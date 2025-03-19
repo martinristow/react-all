eb523e3b172681bce27ae4eadfd37f7972868c09
martinristow <martinristov084@gmail.com>
2025-03-19 20:52:13 +0100

    Improve movie search functionality
    - Added `useEffect` to fetch movie data on component mount
    - Introduced `movieData` state to store API response
    - Updated default movie name to "Baby Driver"
    - Integrated `MovieCard` component for better UI
    - Fixed API request handling for improved error logging

--- roadmap/Movies-API/movies/src/Components/SearchMovie.jsx
+++ roadmap/Movies-API/movies/src/Components/SearchMovie.jsx
@@ -1,18 +1,21 @@
-import React, {useState} from 'react'
+import React, {use, useEffect, useState} from 'react'
 import axios from 'axios'
+import MovieCard from "../Templates/Snippets/MovieCard.jsx";
+
 
 const SearchMovie = () => {
 
-    const [movieName, setMovieName] = useState("")
+    const [movieName, setMovieName] = useState("Baby Driver");
+    const [movieData, setMovieData] = useState({});
 
-    const searchMovie = () => {
+    const searchMovieByTitle = () => {
         const API_KEY = import.meta.env.VITE_OMDBAPI_KEY;
         const API_URL = import.meta.env.VITE_OMDBAPI_URL;
         const FULL_API_URL = API_URL + "t=" + movieName + "&apikey=" + API_KEY;
 
         axios.get(FULL_API_URL)
             .then(response => {
-                console.log(response.data)
+                setMovieData(response.data);
             })
             .catch(error => {
                 console.log(error)
@@ -20,11 +23,20 @@ const SearchMovie = () => {
 
     }
 
+    useEffect(() => {
+        searchMovieByTitle();
+    }, []);
+
+
     return (
-        <form>
-            <input type="text" placeholder="Enter a movie" onInput={e => setMovieName(e.currentTarget.value)}/>
-            <button type="button" onClick={searchMovie}>Search Movie</button>
-        </form>
+        <>
+            <form>
+                <input type="text" placeholder="Enter a movie" onInput={e => setMovieName(e.currentTarget.value)}/>
+                <button type="button" onClick={searchMovieByTitle}>Search Movie</button>
+            </form>
+
+            <MovieCard movie={movieData}/>
+        </>
     )
 }
 export default SearchMovie
