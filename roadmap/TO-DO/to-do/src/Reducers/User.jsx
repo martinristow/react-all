a88dd5dc9148cc244c4c9c0e72683462ff2a3673
martinristow <martinristov084@gmail.com>
2025-03-21 00:19:06 +0100

    Changed text case

--- roadmap/TO-DO/to-do/src/Reducers/User.jsx
+++ roadmap/TO-DO/to-do/src/Reducers/User.jsx
@@ -9,7 +9,7 @@ export const userReducer = (state, action) => {
     switch (action.type) {
         case "SET_USERNAME":
             return {...state, username: action.payload};
-        case "IS_LOGGED_IN":
+        case "SET_IS_LOGGED_IN":
             return {...state, isLoggedIn: action.payload};
         default:
             return state;
