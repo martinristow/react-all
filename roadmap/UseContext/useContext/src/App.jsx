55a0b82ded5f17479de6a623f29dd96a41a34192
martinristow <martinristov084@gmail.com>
2025-03-20 15:47:42 +0100

    Add AmountContext and currency selection to App
    - Created `AmountContext` to manage the input amount
    - Added `updateAmount` function to update the amount state
    - Modified `updateCurrency` to accept a selected currency
    - Implemented an input field for amount entry
    - Added a dropdown (`select`) to choose a currency dynamically
    - Integrated `CURRENCIES` from `CurrencyUtils.jsx` for available currency options

--- roadmap/UseContext/useContext/src/App.jsx
+++ roadmap/UseContext/useContext/src/App.jsx
@@ -1,26 +1,42 @@
 import {createContext, useState} from 'react'
-
 import './App.css'
 import Payment from "./Components/Payment.jsx";
+import {CURRENCIES} from "./Utils/CurrencyUtils.jsx";
 
 export const CurrencyContext = createContext("USD");
+export const AmountContext = createContext(0);
 
 function App() {
 
     const [currency, setCurrency] = useState("USD")
+    const [amount, setAmount] = useState(0)
 
-    const updateCurrency = () => {
-        setCurrency("EURO")
+    const updateCurrency = (currency) => {
+        setCurrency(currency)
     }
 
+    const updateAmount = (value) => {
+        setAmount(value);
+    }
     return (
         <>
             <CurrencyContext.Provider value={{currency, updateCurrency}}>
-                <Payment />
-
-                <button onClick={updateCurrency}>Change Currency</button>
+                <AmountContext.Provider value={{amount, updateAmount}}>
+                    <Payment/>
+                </AmountContext.Provider>
             </CurrencyContext.Provider>
 
+
+            <input type="number" onInput={e => updateAmount(e.currentTarget.value)}/>
+            <select onChange={e => updateCurrency(e.currentTarget.value)}>
+
+                {
+                    Object.keys(CURRENCIES).map((currency) => (
+                        <option key={currency} value={currency}>{currency}</option>
+                    ))
+                }
+
+            </select>
 
         </>
     )
