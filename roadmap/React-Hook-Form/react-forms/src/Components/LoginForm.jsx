38902f296c627f713e23aca3ea3a0ad1faeab392
martinristow <martinristov084@gmail.com>
2025-03-21 20:03:15 +0100

    Added react-hook-form

--- roadmap/React-Hook-Form/react-forms/src/Components/LoginForm.jsx
+++ roadmap/React-Hook-Form/react-forms/src/Components/LoginForm.jsx
@@ -1,34 +1,32 @@
 import React, {useState} from 'react'
+import {useForm} from "react-hook-form"
+
 
 const LoginForm = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: {errors},
+    } = useForm();
 
-    const [password, setPassword] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-
-    const [email, setEmail] = useState('');
-
-    const checkPassword = (newPassword) => {
-        newPassword = newPassword.trim();
-
-        if(newPassword !== "") {
-            setPassword(newPassword);
-            setPasswordError('');
-            console.log(password);
-        } else {
-            setPasswordError("Enter a valid password");
-        }
-
-
-    }
+    const formSubmitted = (data) => console.log(data)
 
     return (
-        <form action="">
+        <form onSubmit={handleSubmit(formSubmitted)}>
 
-            <input type="text" placeholder="Enter your name"/>
-            <h1>{passwordError}</h1>
-            <input onInput={e => checkPassword(e.currentTarget.value)} type="password" placeholder="Enter your password" onChange={e => setPassword(e.currentTarget.value)} />
+            <input {...register("email",
+                {required: true,
+                        pattern: {
+                            value: /\S+@\S+\.\S+/,
+                            message: "Entered value does not match email format",
+                        }})}
+                   type="text" placeholder="Enter your email"/>
+
+            {errors.email && <span>{errors.email.message}</span>}
+            {errors.password && <span>Enter a valid password</span>}
+            <input {...register("password", {required: true})} type="password" placeholder="Enter your password"/>
             <button>Login</button>
-
         </form>
     )
 }
