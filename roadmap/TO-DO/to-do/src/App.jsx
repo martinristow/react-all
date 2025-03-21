a4c6c23b590951f1476035d3923e73c50f9dc7ee
martinristow <martinristov084@gmail.com>
2025-03-21 13:38:43 +0100

    Created useContext, userReducer and added navigation

--- roadmap/TO-DO/to-do/src/App.jsx
+++ roadmap/TO-DO/to-do/src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {createContext, useReducer} from 'react'
 import {BrowserRouter, Route, Routes} from "react-router";
 import Login from "./Pages/Login.jsx";
+import Navigation from "./Partials/Navigation.jsx";
+import {getUsersInitialData, userReducer} from "./Reducers/User.jsx";
 
+
+export const UserContext = createContext();
 
 const App = () => {
+
+
+    const [userState, userDispatch] = useReducer(userReducer, getUsersInitialData())
+
     return (
         <BrowserRouter>
-          <Routes>
-            <Route path="/" />
-            <Route path="/login" element={<Login />} />
-          </Routes>
+            <UserContext.Provider value={{userState, userDispatch}}>
+                <Navigation/>
+                <Routes>
+                    <Route path="/"/>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+
+            </UserContext.Provider>
+
         </BrowserRouter>
 
     )
