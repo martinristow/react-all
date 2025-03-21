fa6d161c84627fc0263cc4d3f051541654e6cb93
martinristow <martinristov084@gmail.com>
2025-03-21 20:40:29 +0100

    Added a few validators

--- roadmap/React-Hook-Form/react-forms/src/Validators/EmailValidator.jsx
+++ roadmap/React-Hook-Form/react-forms/src/Validators/EmailValidator.jsx
@@ -1,5 +1,22 @@
 export const EmailValidator = ({
-        required: true,
+        required: {
+            value: true,
+            message: "Email is required"
+        },
+        minLength: {
+            value: 15,
+            message: "Email address must be at least 15 characters long"
+        },
+
+        validateEmail: value => {
+            const invalidEmails = ["admin@gmail.com", "test@gmail.com"];
+            if (invalidEmails.includes(value)) {
+                return "Email address is invalid";
+            }
+
+            return true;
+        },
+
         pattern: {
             value: /\S+@\S+\.\S+/,
             message: "Entered value does not match email format",
