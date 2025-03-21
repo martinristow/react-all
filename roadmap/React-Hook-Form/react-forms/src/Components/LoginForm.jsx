aa781d5ff4c9951e0fa3abdadb61467e316f9ff3
martinristow <martinristov084@gmail.com>
2025-03-21 19:12:30 +0100

    Simple Form

--- roadmap/React-Hook-Form/react-forms/src/Components/LoginForm.jsx
+++ roadmap/React-Hook-Form/react-forms/src/Components/LoginForm.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const LoginForm = () => {
+
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    const [email, setEmail] = useState('');
+
+    const checkPassword = (newPassword) => {
+        newPassword = newPassword.trim();
+
+        if(newPassword !== "") {
+            setPassword(newPassword);
+            setPasswordError('');
+            console.log(password);
+        } else {
+            setPasswordError("Enter a valid password");
+        }
+
+
+    }
+
     return (
         <form action="">
 
             <input type="text" placeholder="Enter your name"/>
-            <input type="password" placeholder="Enter your password"/>
+            <h1>{passwordError}</h1>
+            <input onInput={e => checkPassword(e.currentTarget.value)} type="password" placeholder="Enter your password" onChange={e => setPassword(e.currentTarget.value)} />
             <button>Login</button>
+
         </form>
     )
 }
