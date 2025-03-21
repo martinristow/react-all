3244e8152190ea3f34fb677a8498d6bf8a97089a
martinristow <martinristov084@gmail.com>
2025-03-22 00:27:44 +0100

    Added routing system

--- roadmap/WeatherApp/weather-app/src/App.jsx
+++ roadmap/WeatherApp/weather-app/src/App.jsx
@@ -1,8 +1,17 @@
-function App() {
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./Pages/Home.tsx";
+
+const App = () => {
 
     return (
         <>
 
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
