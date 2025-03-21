341b8c54f057bcff928ffd6356af56eb5a8ea314
martinristow <martinristov084@gmail.com>
2025-03-21 18:45:30 +0100

    Initial Commit

---
+++ roadmap/React-Hook-Form/react-forms/src/Components/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginForm = () => {
+    return (
+        <form action="">
+
+            <input type="text" placeholder="Enter your name"/>
+            <input type="password" placeholder="Enter your password"/>
+            <button>Login</button>
+        </form>
+    )
+}
+export default LoginForm
