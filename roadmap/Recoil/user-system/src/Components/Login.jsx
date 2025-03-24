ef6ad6fcdd5dd3a68cd2a4a791f40964fcea9eb7
martinristow <martinristov084@gmail.com>
2025-03-25 00:28:26 +0100

    added useSetRecoilState and set a true value if user is successful loggedIn

--- roadmap/Recoil/user-system/src/Components/Login.jsx
+++ roadmap/Recoil/user-system/src/Components/Login.jsx
@@ -1,26 +1,32 @@
 import React, {useState} from 'react'
+import {useSetRecoilState} from "recoil";
+import {userState} from "../States/userState.js";
 
 const Login = () => {
 
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
 
+    const setUserState = useSetRecoilState(userState)
 
 
     const handleLogin = () => {
 
-        if(email !== "admin@admin.com" || password !== "123456"){
+        if (email !== "admin@admin.com" || password !== "123456") {
             console.log("Nope")
             return;
         }
-        console.log("Login")
+        setUserState({
+            "loggedIn": true
+        });
     }
 
     return (
         <>
             <form>
                 <input type="text" placeholder="Enter your email" onInput={e => setEmail(e.currentTarget.value)}/>
-                <input type="password" placeholder="Enter your password" onInput={e => setPassword(e.currentTarget.value)}/>
+                <input type="password" placeholder="Enter your password"
+                       onInput={e => setPassword(e.currentTarget.value)}/>
                 <button type="button" onClick={handleLogin}>Login</button>
             </form>
         </>
