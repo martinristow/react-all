eb60bd3a81412790db5aa65511bd39ad04a79263
martinristow <martinristov084@gmail.com>
2025-03-25 00:15:01 +0100

    added a little logic for login system

--- roadmap/Recoil/user-system/src/Components/Login.jsx
+++ roadmap/Recoil/user-system/src/Components/Login.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
 
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+
+
     const handleLogin = () => {
-        console.log('Login');
+
+        if(email !== "admin@admin.com" || password !== "123456"){
+            console.log("Nope")
+            return;
+        }
+        console.log("Login")
     }
 
     return (
-        <form>
-            <input type="text" placeholder="Enter your email"/>
-            <input type="password" placeholder="Enter your password"/>
-            <button type="button" onClick={handleLogin}>Login</button>
-        </form>
+        <>
+            <form>
+                <input type="text" placeholder="Enter your email" onInput={e => setEmail(e.currentTarget.value)}/>
+                <input type="password" placeholder="Enter your password" onInput={e => setPassword(e.currentTarget.value)}/>
+                <button type="button" onClick={handleLogin}>Login</button>
+            </form>
+        </>
     )
 }
 export default Login
