558f7436e0012d2bd079db263cee6516b1925ef8
martinristow <martinristov084@gmail.com>
2025-03-25 18:53:23 +0100

    Added effects_UNSTANBLE and loaded localStorageEffect

--- roadmap/Recoil/user-system/src/States/userState.js
+++ roadmap/Recoil/user-system/src/States/userState.js
@@ -1,7 +1,11 @@
 import {atom} from "recoil";
+import localStorageEffect from "../Effects/localStorageEffect.js";
 
 
 export const userState = atom({
     key: 'userState',
     default: {},
+    effects_UNSTABLE: [
+    localStorageEffect()
+    ],
 });
