5ef5d3e5c98d4b2eaf4c0a5ecf298bcd3cbb3caa
martinristow <martinristov084@gmail.com>
2025-03-25 20:02:41 +0100

    Add controlled input and task creation logic to CreateTasks component
    Introduced useState to manage the taskName state.
    
    Implemented a controlled input field to ensure synchronization with the taskName state.
    
    Added validation to prevent the creation of tasks with empty or whitespace-only titles.
    
    Cleared the input field after successfully creating a task.
    
    Integrated useSetRecoilState to update the tasksState with new tasks.

--- roadmap/Recoil/user-system/src/Components/CreateTasks.jsx
+++ roadmap/Recoil/user-system/src/Components/CreateTasks.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useSetRecoilState} from "recoil";
+import {tasksState} from "../States/tasksState.js";
 
 const CreateTasks = () => {
+
+    const [taskName, setTaskName] = useState("")
+
+    const setTasks = useSetRecoilState(tasksState)
+
+    const createTask = () => {
+        if (taskName.trim() === "") {
+            return;
+        }
+
+        setTasks(oldTasks => [...oldTasks, taskName]);
+        setTaskName("");
+    };
+
     return (
         <form>
-            <input type='text' placeholder='Enter title of tasks' />
-            <button>Create task</button>
+            <input value={taskName} onInput={e => setTaskName(e.currentTarget.value)} type='text'
+                   placeholder='Enter title of tasks'/>
+            <button type="button" onClick={createTask}>Create task</button>
         </form>
     )
 }
