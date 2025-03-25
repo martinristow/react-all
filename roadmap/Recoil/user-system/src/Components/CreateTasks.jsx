7d96bc9a5d0b215e6fac334ca31a859cc50a8d68
martinristow <martinristov084@gmail.com>
2025-03-25 19:26:56 +0100

    Created a simple form for tasks

---
+++ roadmap/Recoil/user-system/src/Components/CreateTasks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateTasks = () => {
+    return (
+        <form>
+            <input type='text' placeholder='Enter title of tasks' />
+            <button>Create task</button>
+        </form>
+    )
+}
+export default CreateTasks
