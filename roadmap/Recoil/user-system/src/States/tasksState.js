9646c6026e186e402d201d49e56a78c8767dd14c
martinristow <martinristov084@gmail.com>
2025-03-25 20:03:36 +0100

    Add tasksState atom with persistence using userStateEffect
    Created tasksState atom to manage the state of tasks within the application.
    
    Integrated userStateEffect to enable synchronization of tasksState with local storage.
    
    Set the default state for tasksState to an empty array.
    
    Ensured persistence of tasks data across sessions using Recoil's effects_UNSTABLE.

--- roadmap/Recoil/user-system/src/States/tasksState.js
+++ roadmap/Recoil/user-system/src/States/tasksState.js
@@ -1,6 +1,10 @@
 import {atom} from "recoil";
+import userStateEffect from "../Effects/userStateEffect.js";
 
 export const tasksState = atom({
     key: "tasksState",
     default: [],
+    effects_UNSTABLE: [
+        userStateEffect
+    ]
 })
