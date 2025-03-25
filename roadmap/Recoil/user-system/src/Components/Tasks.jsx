94f794366b6aa6d11fcd3b50c4871c7b5c6cf552
martinristow <martinristov084@gmail.com>
2025-03-25 20:04:08 +0100

    Enhance Tasks component with task deletion functionality
    Added task deletion functionality to the Tasks component.
    
    Implemented deleteTask function to remove tasks by index from the tasksState atom.
    
    Updated rendering logic to display tasks dynamically from tasksState.
    
    Ensured proper state management with useRecoilValue and useSetRecoilState hooks.
    
    Preserved the existing functionality for task creation via CreateTasks component.

--- roadmap/Recoil/user-system/src/Components/Tasks.jsx
+++ roadmap/Recoil/user-system/src/Components/Tasks.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
-import {useRecoilValue} from "recoil";
+import {useRecoilValue, useSetRecoilState} from "recoil";
 import {userState} from "../States/userState.js";
 import CreateTasks from "./CreateTasks.jsx";
+import {tasksState} from "../States/tasksState.js";
 
 const Tasks = () => {
 
     const userData = useRecoilValue(userState);
+    const taskData = useRecoilValue(tasksState);
+    const setTaskData = useSetRecoilState(tasksState);
 
+    const deleteTask = (taskIndex) => {
+        const filteredTasks = taskData.filter((_, index) => index !== taskIndex);
+        setTaskData(filteredTasks);
+
+    }
     return (
-        userData.loggedIn && (
-            <div>
-                <CreateTasks/>
-                <p>Tasks</p>
-            </div>
-        )
+
+        <>
+            {
+                taskData.map((taskName, index) => {
+                    return (
+                        <div key={index}>
+                            <p>{taskName}</p>
+                            <button onClick={() => deleteTask(index)} key={index}>Delete task</button>
+                        </div>
+                    )
+                })
+            }
+
+            {userData.loggedIn && (
+                <div>
+                    <CreateTasks/>
+                    <p>Tasks</p>
+                </div>
+            )}
+        </>
 
     )
 }
