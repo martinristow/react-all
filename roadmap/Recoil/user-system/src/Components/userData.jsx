b724a41a7b479438e633fc044c276bd26e31e5fa
martinristow <martinristov084@gmail.com>
2025-03-25 17:36:02 +0100

    Added UserData component to get all data back and print their emails on frontend

---
+++ roadmap/Recoil/user-system/src/Components/userData.jsx
@@ -0,0 +1,14 @@
+import {useRecoilValue} from "recoil";
+import {userState} from "../States/userState.js";
+
+const UserData = () => {
+    const userData = useRecoilValue(userState);
+    console.log(userData)
+
+    return (
+        <p>{userData.email}</p>
+    )
+};
+
+
+export default UserData;
