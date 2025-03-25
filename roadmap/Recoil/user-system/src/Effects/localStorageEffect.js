7a4ade596d95622d647c281b34b328a4d22b3dcb
martinristow <martinristov084@gmail.com>
2025-03-25 23:51:50 +0100

    made this function reusable

--- roadmap/Recoil/user-system/src/Effects/localStorageEffect.js
+++ roadmap/Recoil/user-system/src/Effects/localStorageEffect.js
@@ -1,16 +1,15 @@
 import {useRecoilValue} from "recoil";
 
-const localStorageEffect = () => ({setSelf, onSet}) => {
-    const savedValues = localStorage.getItem("userData");
+export const localStorageEffect = (localStorageKey) => ({setSelf, onSet}) => {
+    const savedValues = localStorage.getItem(localStorageKey);
 
     if (savedValues !== null) {
         setSelf(JSON.parse(savedValues));
     }
 
     onSet(newValue => {
-        localStorage.setItem("userData", JSON.stringify(newValue));
+        localStorage.setItem(localStorageKey, JSON.stringify(newValue));
     })
 
 }
 
-export default localStorageEffect()
