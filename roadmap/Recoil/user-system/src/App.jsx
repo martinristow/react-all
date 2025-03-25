e0c2f3fb7da67f854512bf2121e6c68d52b5c7cf
martinristow <martinristov084@gmail.com>
2025-03-25 17:36:55 +0100

    Loaded UserData component

--- roadmap/Recoil/user-system/src/App.jsx
+++ roadmap/Recoil/user-system/src/App.jsx
@@ -1,11 +1,13 @@
 import {RecoilRoot} from "recoil";
 import Login from "./Components/Login.jsx";
+import UserData from "./Components/userData.jsx";
 
 function App() {
 
     return (
         <>
             <RecoilRoot>
+                <UserData />
                 <Login />
 
             </RecoilRoot>
