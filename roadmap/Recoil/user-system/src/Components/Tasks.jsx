00c701c5c935bec08a72ee7c1a84228d73128413
martinristow <martinristov084@gmail.com>
2025-03-26 21:00:20 +0100

    Merge current comments with a new comments and implement a callback function for comment form

--- roadmap/Recoil/user-system/src/Components/Tasks.jsx
+++ roadmap/Recoil/user-system/src/Components/Tasks.jsx
@@ -5,6 +5,8 @@ import CreateTasks from "./CreateTasks.jsx";
 import {tasksState} from "../States/tasksState.js";
 import {categories} from "../Utils/Categories.js";
 import {useForm} from "react-hook-form";
+import CommentForm from "./CommentForm.jsx";
+import GetAllCommentsForPost from "./GetAllCommentsForPost.jsx";
 
 const Tasks = () => {
 
@@ -56,45 +58,69 @@ const Tasks = () => {
         setTaskData(updateTasks);
     }
 
+    const newCommentPosted = (data) =>{
+        // console.log(data)
+
+        const tasksWithComments = taskData.map(task => {
+            if(task.id === parseInt(data.taskId)) {
+
+                let existingComments = [];
+                if(Array.isArray(task.comments)) {
+                    existingComments = task.comments;
+                }
+                return {
+                  ...task,
+                  comments: [...existingComments, data.comments],
+                };
+            }
+            return task;
+        })
+        setTaskData(tasksWithComments);
+    }
+
     return (
 
         <>
 
             {userData.loggedIn && (
-                    taskData.map((task, index) => {
-                        return (
-                            <div key={task.id}>
+                taskData.map((task, index) => {
+                    return (
+                        <div key={task.id}>
 
-                                {editTaskId === task.id ? (
-                                    <form onSubmit={handleSubmit(updateTask)}>
-                                        <input {...register("taskId")} type="hidden" defaultValue={task.id}/>
-                                        <input {...register("taskName")} type="text" defaultValue={task.name}/>
-                                        <select defaultValue={task.category} {...register("category")}>
-                                            {
-                                                categories.map((category, index) => {
-                                                    return (
-                                                        <option key={index}
-                                                                value={category}>{category}</option>
-                                                    )
-                                                })
-                                            }
-                                        </select>
-                                        <button>Update Task</button>
-                                    </form>
+                            {editTaskId === task.id ? (
+                                <form onSubmit={handleSubmit(updateTask)}>
+                                    <input {...register("taskId")} type="hidden" defaultValue={task.id}/>
+                                    <input {...register("taskName")} type="text" defaultValue={task.name}/>
+                                    <select defaultValue={task.category} {...register("category")}>
+                                        {
+                                            categories.map((category, index) => {
+                                                return (
+                                                    <option key={index}
+                                                            value={category}>{category}</option>
+                                                )
+                                            })
+                                        }
+                                    </select>
+                                    <button>Update Task</button>
+                                </form>
 
-                                ) : (
-                                    <div>
-                                        <p onClick={() => setEditTaskId(task.id)}>{task.name}</p>
-                                        <p onClick={() => setEditTaskId(task.id)}>{task.category}</p>
-                                        <button onClick={() => deleteTask(index)} key={index}>Delete task</button>
+                            ) : (
+                                <div>
+                                    <p onClick={() => setEditTaskId(task.id)}>{task.name}</p>
+                                    <p onClick={() => setEditTaskId(task.id)}>{task.category}</p>
 
-                                    </div>
-                                )}
+                                    <CommentForm task={task} callbackPostComment={newCommentPosted}/>
 
+                                    <button onClick={() => deleteTask(index)} key={index}>Delete task</button>
 
-                            </div>
-                        )
-                    })
+                                </div>
+                            )}
+
+                            <GetAllCommentsForPost comments={task.comments}/>
+
+                        </div>
+                    )
+                })
             )}
 
 
