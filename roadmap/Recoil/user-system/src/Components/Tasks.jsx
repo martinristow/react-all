d55ac833ce383297f9d2dabd0aa39830bc1e61a7
martinristow <martinristov084@gmail.com>
2025-03-26 19:22:08 +0100

    Added logic for find the current task and updated

--- roadmap/Recoil/user-system/src/Components/Tasks.jsx
+++ roadmap/Recoil/user-system/src/Components/Tasks.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useRecoilValue, useSetRecoilState} from "recoil";
 import {userState} from "../States/userState.js";
 import CreateTasks from "./CreateTasks.jsx";
 import {tasksState} from "../States/tasksState.js";
+import {categories} from "../Utils/Categories.js";
+import {useForm} from "react-hook-form";
 
 const Tasks = () => {
 
     const userData = useRecoilValue(userState);
     const taskData = useRecoilValue(tasksState);
     const setTaskData = useSetRecoilState(tasksState);
+    const [editTaskId, setEditTaskId] = useState(null);
+
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: {errors},
+        reset,
+    } = useForm()
+
+    useEffect(() => {
+        if (editTaskId !== undefined) {
+            const task = taskData.find(t => t.id === editTaskId);
+
+            if (task) {
+                reset({
+                    taskName: task.name,
+                    category: task.category,
+                });
+            }
+        }
+    }, [editTaskId, reset, taskData]);
 
     const deleteTask = (taskIndex) => {
         const filteredTasks = taskData.filter((_, index) => index !== taskIndex);
         setTaskData(filteredTasks);
+    }
+
+    const updateTask = (data) => {
+        console.log(data);
     }
 
     return (
@@ -22,9 +50,32 @@ const Tasks = () => {
                 taskData.map((task, index) => {
                     return (
                         <div key={task.id}>
-                            <p>{task.name}</p>
-                            <p>{task.category}</p>
-                            <button onClick={() => deleteTask(index)} key={index}>Delete task</button>
+
+                            {editTaskId === task.id ? (
+                                <form onSubmit={handleSubmit(updateTask)}>
+                                    <input {...register("taskName")} type="text" defaultValue={task.name}/>
+                                    <select defaultValue={task.category} {...register("category")}>
+                                        {
+                                            categories.map((category, index) => {
+                                                return (
+                                                    <option key={index}
+                                                            value={category}>{category}</option>
+                                                )
+                                            })
+                                        }
+                                    </select>
+                                    <button>Update Task</button>
+                                </form>
+
+                            ) : (
+                                <div>
+                                    <p onClick={() => setEditTaskId(task.id)}>{task.name}</p>
+                                    <p onClick={() => setEditTaskId(task.id)}>{task.category}</p>
+                                    <button onClick={() => deleteTask(index)} key={index}>Delete task</button>
+
+                                </div>
+                            )}
+
 
                         </div>
                     )
