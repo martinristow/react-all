16f75a854f4c09f2ec982ceaec382b946b54fc5c
martinristow <martinristov084@gmail.com>
2025-03-26 23:09:44 +0100

    Update GetAllCommentsForPost component to use Recoil state for managing comments
    Integrated useRecoilValue and useSetRecoilState to manage the comments state via Recoil.
    
    Added logic to delete a specific comment from a task using taskId for identification.
    
    Updated the deleteComment function to modify the tasksState atom directly.
    
    Ensured proper handling of the comments array with a fallback check using Array.isArray.
    
    This update improves state management and aligns the component with Recoil's state handling strategy.

--- roadmap/Recoil/user-system/src/Components/GetAllCommentsForPost.jsx
+++ roadmap/Recoil/user-system/src/Components/GetAllCommentsForPost.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import {tasksState} from "../States/tasksState.js";
 
-const GetAllCommentsForPost = ({comments}) => {
+const GetAllCommentsForPost = ({comments, taskId}) => {
 
-    const deleteComment = (comment) => {
-        console.log(comment)
+    const taskData = useRecoilValue(tasksState);
+    const setTaskData = useSetRecoilState(tasksState);
+
+    const deleteComment = (comment, taskId) => {
+
+        const tasksWithComments = taskData.map(task => {
+            if (task.id === parseInt(taskId)) {
+                const updatedComments = task.comments.filter(taskComment => taskComment !== comment);
+
+                return {
+                    ...task,
+                    comments: updatedComments,
+                }
+            }
+            return task;
+        });
+        setTaskData(tasksWithComments);
     }
 
     return (
         <>
-            {
-                comments.map((comment, index) => (
-                    <div key={index}>
-                        <p>{comment}</p>
-                        <button onClick={() => deleteComment(comment)}>Delete Comment</button>
-                    </div>
-
-                ))
-            }
+            {Array.isArray(comments) && comments.map((comment, index) => (
+                <div key={index}>
+                    <p>{comment}</p>
+                    <button onClick={() => deleteComment(comment, taskId)}>Delete Comment</button>
+                </div>
+            ))}
         </>
     );
+
 }
 export default GetAllCommentsForPost
