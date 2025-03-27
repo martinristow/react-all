d7ec1345a7f663c6a0772abefb4ce1c6e5dab9b4
martinristow <martinristov084@gmail.com>
2025-03-27 20:25:23 +0100

    removed before images and added new one for showing in about page

--- next-js-roadmap/next-second/src/app/about/page.js
+++ next-js-roadmap/next-second/src/app/about/page.js
@@ -1,8 +1,16 @@
-
+import Image from "next/image";
+import F1_Image from "../../../public/images/f1-image.jpg"
+console.log(F1_Image)
 export default function About(){
     return (
         <>
             <p>About Page</p>
+
+            <Image src={F1_Image}
+                   alt="Formula 1 Image"
+                   width="500"
+                   height="500"
+            />
         </>
     )
 }
