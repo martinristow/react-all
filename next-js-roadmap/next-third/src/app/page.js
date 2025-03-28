49cd064b07c15ec6733ef0dfa06f5c60cd78c17d
martinristow <martinristov084@gmail.com>
2025-03-28 18:40:47 +0100

    Initial Commit

---
+++ next-js-roadmap/next-third/src/app/page.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default async function Home() {
+
+    const response = await fetch('https://dummyjson.com/products?limit=9');
+    const data = await response.json();
+
+    return (
+        <div>
+            <h1 style={{ textAlign: "center", margin: "20px 0", color: "#333" }}>Our Products</h1>
+
+            <div className="products-container">
+                {data.products.map(product => (
+                    <div key={product.id} className="product-card">
+                        <Image
+                            src={product.thumbnail}
+                            alt={product.title}
+                            width={200}
+                            height={200}
+                            className="product-image"
+                        />
+                        <div className="product-info">
+                            <h1>{product.title}</h1>
+                            <p>{product.description}</p>
+                            <Link href={`/products/${product.id}`}>
+                                <button type="button">See more</button>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
