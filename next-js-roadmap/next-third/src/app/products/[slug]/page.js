cf9c11bf3d900902e53c7deed622916629a3d794
martinristow <martinristov084@gmail.com>
2025-03-28 18:43:53 +0100

    got current product by ID and displayed their data

---
+++ next-js-roadmap/next-third/src/app/products/[slug]/page.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default async function ProductsSlug({params}) {
+
+    const urlID = parseInt(params.slug)
+    if (Number.isNaN(urlID)) {
+        return (
+            <>
+                <h1>Please enter a number!</h1>
+            </>
+        )
+    }
+
+    const response = await fetch('https://dummyjson.com/products/' + urlID);
+    const data = await response.json();
+
+    if (data.message) {
+        return (
+            <>
+                <h1>{data.message}!</h1>
+            </>
+        )
+    }
+
+    return (
+        <div className="products">
+            <h1 className="product-title">{data.title}</h1>
+            <Image src={data.thumbnail} alt={data.title} width={300} height={300}/>
+            <p className="product-description">{data.description}</p>
+            <p className="product-price">{data.price}</p>
+        </div>
+    )
+}
