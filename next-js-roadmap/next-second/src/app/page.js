dfa9b2f85248afb3302d8927f26a6d5e88844c88
martinristow <martinristov084@gmail.com>
2025-03-28 14:30:25 +0100

    fetching data from dummy json and displayed a dummy data on page

--- next-js-roadmap/next-second/src/app/page.js
+++ next-js-roadmap/next-second/src/app/page.js
@@ -1,10 +1,28 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import './globals.css';
 
-export default function Home() {
-  return (
-  <>
-  Hi
-  </>
-  );
-}
+export default async function Home() {
+
+    const response = await fetch('https://dummyjson.com/products?limit=9');
+    const data = await response.json();
+
+    return (
+        <>
+            Products
+            <div className="productWrapper">
+                {
+                    data.products.map((product) => (
+                        <div className="product" key={product.id}>
+                            <Image key={product.id} src={product.thumbnail} alt="asd" width="200" height="200"/>
+                            <h1>{product.title}</h1>
+                            <p>{product.description}</p>
+                            <p className="product-price">{product.price}</p>
+
+                            <br/>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    );
+};
