b49ed72f18e06cbffc10ab4b95ee1a5935380a26
martinristow <martinristov084@gmail.com>
2025-03-29 20:38:11 +0100

    added a new function to add a Product in dummy json

--- next-js-roadmap/next-third/src/services/productService.js
+++ next-js-roadmap/next-third/src/services/productService.js
@@ -31,3 +31,16 @@ export async function searchProductByQuery(query) {
     return await response.json();
 }
 
+
+export function addProduct(productName, productDescription, productPrice) {
+    fetch('https://dummyjson.com/products/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: productName,
+            description: productDescription,
+            price: productPrice,
+        })
+    })
+}
+
