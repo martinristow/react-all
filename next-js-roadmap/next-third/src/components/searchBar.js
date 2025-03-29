6b1870565a4d39d777fc5695031eeea2b6d8c5ed
martinristow <martinristov084@gmail.com>
2025-03-29 17:58:25 +0100

    - added a button to call search function -  input field for search is changed to onInput - added to useStates to save searchTerm and searchResult - displayed data when client will click on search button

--- next-js-roadmap/next-third/src/components/searchBar.js
+++ next-js-roadmap/next-third/src/components/searchBar.js
@@ -1,17 +1,33 @@
 'use client'
 
+import useState from "react";
+
 const SearchBar = () => {
 
-    const search = (searchTerm) => {
-        console.log(searchTerm)
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResult, setSearchResult] = useState([]);
+
+    const search = async () => {
+
+        const response = await fetch('http://localhost:3000/api/search?prebaruvanje=' + searchTerm);
+        const data = await response.json();
+        setSearchResult(data.products);
     }
 
     return (
         <div>
             <form>
                 <input type="text" placeholder="Enter a name of product"
-                       onChange={e => search(e.target.value)}/>
+                       onInput={e => setSearchTerm(e.target.value)}/>
+                <button type="button" onClick={search}>Search</button>
             </form>
+
+            {
+                searchResult && searchResult.map((product, index) => (
+                    <p key={index}>{product.title}</p>
+                ))
+            }
+
         </div>
     )
 }
