57c1f787650a13f2d92e463763a32b406bb477eb
martinristow <martinristov084@gmail.com>
2025-03-29 21:03:23 +0100

    login user and get tokens

--- next-js-roadmap/next-third/pages/api/auth_user.js
+++ next-js-roadmap/next-third/pages/api/auth_user.js
@@ -1,5 +1,20 @@
 // api/register
 
 export default async function handler(req, res){
+    fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+
+            username: 'emilys',
+            password: 'emilyspass',
+            expiresInMins: 30, // optional, defaults to 60
+        }),
+        credentials: 'include' // Include cookies (e.g., accessToken) in the request
+    })
+        .then(res => res.json())
+        .then(console.log);
+
     res.status(200).json({message: "success"});
+
 }
