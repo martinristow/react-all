7ccce4f276ea0aed3db3f137d4fcca811e2ec875
martinristow <martinristov084@gmail.com>
2025-03-29 23:12:44 +0100

    implement handling errors

--- next-js-roadmap/next-third/pages/api/auth_user.js
+++ next-js-roadmap/next-third/pages/api/auth_user.js
@@ -1,20 +1,32 @@
 // api/register
 
-export default async function handler(req, res){
-    fetch('https://dummyjson.com/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
+export default async function handler(req, res) {
 
-            username: 'emilys',
-            password: 'emilyspass',
-            expiresInMins: 30, // optional, defaults to 60
-        }),
-        credentials: 'include' // Include cookies (e.g., accessToken) in the request
-    })
-        .then(res => res.json())
-        .then(console.log);
+    console.log(req.body)
+    try {
+        const response = await fetch('https://dummyjson.com/auth/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
 
-    res.status(200).json({message: "success"});
+                username: req.body.username,
+                password: req.body.password,
+                expiresInMins: 30, // optional, defaults to 60
+            }),
+            credentials: 'include' // Include cookies (e.g., accessToken) in the request
+        })
+
+        const data = await response.json()
+
+        res.status(200).json({
+            message: "success",
+            token: data.accessToken
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+
 
 }
