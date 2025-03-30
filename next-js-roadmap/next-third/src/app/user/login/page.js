0a420fc54a58b3061c994beccc8e9306c689be37
martinristow <martinristov084@gmail.com>
2025-03-30 20:39:19 +0200

    removed dummy json post method and added firebase sign in with email and password

--- next-js-roadmap/next-third/src/app/user/login/page.js
+++ next-js-roadmap/next-third/src/app/user/login/page.js
@@ -1,45 +1,36 @@
 'use client'
 
 import React, {useState} from 'react'
+import {useAuth} from "@/app/context/authContext";
+import {auth} from "@/app/firebase"
+import {signInWithEmailAndPassword } from "firebase/auth"
 
 const UserLogin = () => {
 
-    const [username, setUsername] = useState("");
+
+    const {loggedIn} = useAuth()
+
+    if(loggedIn){
+        window.location.href = "/";
+    }
+
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const handleLogin = async () => {
-        if (!username || !password) {
+        if (!email || !password) {
             alert("Enter a username and password");
             return;
         }
 
-        const response = await fetch("/api/auth_user", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password,
-            })
-        })
-
-
-        if (response.status !== 200) {
-            alert("Something went wrong! Try again");
-            return;
-        }
-
-        const data = await response.json()
-        console.log(data)
-        localStorage.setItem("token", data.token)
+        signInWithEmailAndPassword(auth, email, password);
 
     }
 
     return (
         <div>
             <form>
-                <input type="text" placeholder="Enter a username" onInput={e => setUsername(e.currentTarget.value)}/>
+                <input type="email" placeholder="Enter a email" onInput={e => setEmail(e.currentTarget.value)}/>
                 <input type="password" placeholder="Enter a password"
                        onInput={e => setPassword(e.currentTarget.value)}/>
                 <button type="button" onClick={handleLogin}>Login</button>
