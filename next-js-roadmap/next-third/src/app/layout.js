bbd95673ddce6d6b0653e9b53c6af4006e2848c0
martinristow <martinristov084@gmail.com>
2025-03-30 16:26:04 +0200

    Added auth provider

--- next-js-roadmap/next-third/src/app/layout.js
+++ next-js-roadmap/next-third/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SearchBar from "@/components/searchBar";
+import {AuthProvider} from "@/app/context/authContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
       {<SearchBar/>}
+      <AuthProvider>
         {children}
+      </AuthProvider>
       </body>
     </html>
   );
