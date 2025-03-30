01f9e27de35e8a603a93eb55ad47c412437ae829
martinristow <martinristov084@gmail.com>
2025-03-30 15:38:53 +0200

    Register page with firebase

---
+++ next-js-roadmap/next-third/src/app/user/register/page.js
@@ -0,0 +1,25 @@
+'use client'
+
+import {auth} from "@/app/firebase";
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {useState} from "react";
+
+export default function UserRegister() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const register = async () => {
+
+        console.log(password, email);
+        await createUserWithEmailAndPassword(auth, email, password);
+
+    }
+
+    return <>
+        <h1>Register Form</h1>
+        <input onInput={e => setEmail(e.currentTarget.value)} type="email" placeholder="Enter your email address"/>
+        <input onInput={e => setPassword(e.currentTarget.value)} type="password" placeholder="Enter your password"/>
+        <button onClick={register}>Register</button>
+    </>
+}
