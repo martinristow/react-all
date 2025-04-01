34ccd81b61cbf23a1426db3b6244c3fa7ebc006b
martinristow <martinristov084@gmail.com>
2025-04-01 18:25:30 +0200

    moved into a new folder, just to clean up file structure

--- roadmap-v1/react-demos/src/App.jsx
+++ roadmap-v1/react-demos/src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import Greet from "./Components/Greet.jsx";
-import Header from "./Components/Header.jsx";
-import Footer from "./Components/Footer.jsx";
-import MainContent from "./Components/MainContent.jsx";
-import WelcomeMessage from "./Components/WelcomeMessage.jsx";
-import JSXRules from "./Components/JSXRules.jsx";
-import ExpressionsInJSX from "./Components/ExpressionsInJSX.jsx";
-import Greeting from "./Components/Greeting.jsx";
-import ProductInfo from "./Components/ProductInfo.jsx";
-import Lists from "./Components/Lists.jsx";
-import UserList from "./Components/UserList.jsx";
-import ProductList from "./Components/ProductList.jsx";
+import Greet from "./Components/Components-Expressions-JSX-Lists/Greet.jsx";
+import Header from "./Components/Components-Expressions-JSX-Lists/Header.jsx";
+import Footer from "./Components/Components-Expressions-JSX-Lists/Footer.jsx";
+import MainContent from "./Components/Components-Expressions-JSX-Lists/MainContent.jsx";
+import WelcomeMessage from "./Components/Components-Expressions-JSX-Lists/WelcomeMessage.jsx";
+import JSXRules from "./Components/Components-Expressions-JSX-Lists/JSXRules.jsx";
+import ExpressionsInJSX from "./Components/Components-Expressions-JSX-Lists/ExpressionsInJSX.jsx";
+import Greeting from "./Components/Components-Expressions-JSX-Lists/Greeting.jsx";
+import ProductInfo from "./Components/Components-Expressions-JSX-Lists/ProductInfo.jsx";
+import Lists from "./Components/Components-Expressions-JSX-Lists/Lists.jsx";
+import UserList from "./Components/Components-Expressions-JSX-Lists/UserList.jsx";
+import ProductList from "./Components/Components-Expressions-JSX-Lists/ProductList.jsx";
 
 const App = () => {
     return (
         <section>
-            <div>
-                {/*<h1>Hello World</h1>*/}
-            </div>
             {/*<Greet/>*/}
             {/*<Header/>*/}
             {/*<MainContent/>*/}
@@ -27,8 +24,9 @@ const App = () => {
             {/*<Greeting/>*/}
             {/*<ProductInfo/>*/}
             {/*<Lists/>*/}
-            <UserList/>
-            <ProductList/>
+            {/*<UserList/>*/}
+            {/*<ProductList/>*/}
+
             {/*<Footer/>*/}
         </section>
     )
