c63d2e7a445e33a99d8704548606859b6452b666
martinristow <martinristov084@gmail.com>
2025-04-04 00:51:17 +0200

    Removed bad code(Data.Consumer) and just improved with useContext and now it works perfectly

--- roadmap-v1/react-demos/src/Components/Hooks/ContextAPI/ComponentC.jsx
+++ roadmap-v1/react-demos/src/Components/Hooks/ContextAPI/ComponentC.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
+import {useContext} from "react";
 import {Data, Data1} from "../../../App.jsx";
 
 const ComponentC = () => {
 
+    const userName = useContext(Data)
+    const userAge = useContext(Data1)
 
     return (
         <div>
-            <Data.Consumer>
-                {
-                    (name) => {
-                        // return <h1>{name}, {age}</h1>
-                        return (
-                            <Data1.Consumer>
-                                {
-                                    (age) => {
-                                        return <h2>My name is {name} and I'm {age} years old.</h2>
-                                    }
-                                }
-                            </Data1.Consumer>
-                        )
-                    }
-                }
-            </Data.Consumer>
+        <h1>My name is {userName} and I'm {userAge} years old.</h1>
         </div>
     )
 }
