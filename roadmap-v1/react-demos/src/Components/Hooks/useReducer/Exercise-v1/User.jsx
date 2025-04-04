4ab150be6d7e0a9f1d48e1815a02ccd845bb1c05
martinristow <martinristov084@gmail.com>
2025-04-04 17:07:24 +0200

    form for data

--- roadmap-v1/react-demos/src/Components/Hooks/useReducer/Exercise-v1/User.jsx
+++ roadmap-v1/react-demos/src/Components/Hooks/useReducer/Exercise-v1/User.jsx
@@ -4,9 +4,23 @@ import {userReducer, initialState} from './userReducer'
 const User = () => {
 
     const [state, dispatch] = useReducer(userReducer, initialState)
-    console.log(state)
+
     return (
-        <div>User</div>
+        <div>
+            <div>
+                <h1>User Profile</h1>
+                <p>{state.name}</p>
+                <p>{state.email}</p>
+                <p>{state.password}</p>
+            </div>
+
+            <div>
+                <h2>Update Profile</h2>
+                <input type="text" placeholder="Enter a name" onInput={e => dispatch({type: 'SET_NAME', payload:e.currentTarget.value})}/>
+                <input type="email" placeholder="Enter a email" onChange={e => dispatch({type: 'SET_EMAIL', payload:e.currentTarget.value})}/>
+                <input type="password" placeholder="Enter a password" onChange={e => dispatch({type: 'SET_PASSWORD', payload:e.currentTarget.value})}/>
+            </div>
+        </div>
     )
 }
 export default User
