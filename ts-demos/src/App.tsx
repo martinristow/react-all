ee3006c1218e218d2e97e43e0cb6255aec21b183
martinristow <martinristov084@gmail.com>
2025-04-09 19:27:20 +0200

    Update App component to include new components
    - Imported and rendered Counter, UserProfile, and TodoList components
    - Commented out previously used components (User, Button, UserInfo, AdminInfo)
    - Cleaned up imports and organized layout

--- ts-demos/src/App.tsx
+++ ts-demos/src/App.tsx
@@ -2,31 +2,39 @@
 // import Button from "./Components/Button.tsx";
 
 
-import UserInfo from "./Components/UserInfo.tsx";
-import AdminInfo from "./Components/AdminInfo.tsx";
-import {Info, AdminInfoList} from "./types.ts";
+// import UserInfo from "./Components/UserInfo.tsx";
+// import AdminInfo from "./Components/AdminInfo.tsx";
+// import {Info, AdminInfoList} from "./types.ts";
+
+import Counter from "./Components/Counter.tsx";
+import UserProfile from "./Components/UserProfile.tsx";
+import TodoList from "./Components/TodoList.tsx";
 
 const App = () => {
-    const user: Info = {
-        id: 1,
-        name: "Martin",
-        email: "martin@gmail.com",
-    }
-
-    const admin: AdminInfoList = {
-        id: 2,
-        name: "Admin",
-        email: "admin@gmail.com",
-        role: "admin",
-        lastLogin: new Date(),
-    }
+    // const user: Info = {
+    //     id: 1,
+    //     name: "Martin",
+    //     email: "martin@gmail.com",
+    // }
+    //
+    // const admin: AdminInfoList = {
+    //     id: 2,
+    //     name: "Admin",
+    //     email: "admin@gmail.com",
+    //     role: "admin",
+    //     lastLogin: new Date(),
+    // }
 
     return (
         <div>
             {/*<User name="martin" age={20} isStudent={true}/>*/}
             {/*<Button label="Click" onClick={()=> {console.log("Clicked")}} disabled={false} />*/}
-            <UserInfo user={user}/>
-            <AdminInfo admin={admin}/>
+            {/*<UserInfo user={user}/>*/}
+            {/*<AdminInfo admin={admin}/>*/}
+
+            <Counter/>
+            <UserProfile/>
+            <TodoList/>
         </div>
     )
 }
