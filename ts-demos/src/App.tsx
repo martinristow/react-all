e8a49497c407c9b26f1b4783d4b741b50a4d23c6
martinristow <martinristov084@gmail.com>
2025-04-09 18:51:09 +0200

    Update App component to use UserInfo and AdminInfo
    - Imported UserInfo and AdminInfo components
    - Imported Info and AdminInfoList types
    - Created user and admin objects with proper types
    - Rendered both components with mock data
    - Commented out old User and Button components

--- ts-demos/src/App.tsx
+++ ts-demos/src/App.tsx
@@ -1,14 +1,32 @@
-import User from "./Components/User.tsx";
-import Button from "./Components/Button.tsx";
+// import User from "./Components/User.tsx";
+// import Button from "./Components/Button.tsx";
 
+
+import UserInfo from "./Components/UserInfo.tsx";
+import AdminInfo from "./Components/AdminInfo.tsx";
+import {Info, AdminInfoList} from "./types.ts";
 
 const App = () => {
+    const user: Info = {
+        id: 1,
+        name: "Martin",
+        email: "martin@gmail.com",
+    }
 
+    const admin: AdminInfoList = {
+        id: 2,
+        name: "Admin",
+        email: "admin@gmail.com",
+        role: "admin",
+        lastLogin: new Date(),
+    }
 
     return (
         <div>
             {/*<User name="martin" age={20} isStudent={true}/>*/}
-            <Button label="Click" onClick={()=> {console.log("Clicked")}} disabled={false} />
+            {/*<Button label="Click" onClick={()=> {console.log("Clicked")}} disabled={false} />*/}
+            <UserInfo user={user}/>
+            <AdminInfo admin={admin}/>
         </div>
     )
 }
