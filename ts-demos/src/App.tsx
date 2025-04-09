4ee75e25246f3ae89715874fed7f185ef4cd3dec
martinristow <martinristov084@gmail.com>
2025-04-09 17:39:05 +0200

    Create App component
    - Added App.tsx file
    - Used the User component inside App
    - Passed name, age, and isStudent as props

--- ts-demos/src/App.tsx
+++ ts-demos/src/App.tsx
@@ -1,6 +1,10 @@
+import User from "./Components/User.tsx";
+
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <User name="martin" age={20} isStudent={true}/>
+        </div>
     )
 }
 export default App
