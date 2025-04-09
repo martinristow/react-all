bf79053a31e6e2dcf12b538bd582b584d55dd3f4
martinristow <martinristov084@gmail.com>
2025-04-09 23:44:36 +0200

    wrap App with MyProvider in main.tsx
    - Ensured global context is available throughout the app
    - Wrapped App component with MyProvider inside React.StrictMode

--- ts-demos/src/main.tsx
+++ ts-demos/src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
+import MyProvider from "./Context/MyContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <MyProvider>
+            <App/>
+        </MyProvider>
+
+    </StrictMode>,
 )
