b7c2a8e66f2676a771d9703ddc5df174368c6c8c
martinristow <martinristov084@gmail.com>
2025-04-10 20:26:07 +0200

    Testing tailwind css code

--- react-tailwind-css/src/App.jsx
+++ react-tailwind-css/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+        </div>
     )
 }
 export default App
