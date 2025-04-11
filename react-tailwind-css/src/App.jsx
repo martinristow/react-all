13fbc0bf36749addf248eddfe06c753db2ee6c2c
martinristow <martinristov084@gmail.com>
2025-04-11 11:45:42 +0200

    Added daisy ui buttons

--- react-tailwind-css/src/App.jsx
+++ react-tailwind-css/src/App.jsx
@@ -4,6 +4,14 @@ const App = () => {
     return (
         <div>
             <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+            <button className="btn btn-soft">Default</button>
+            <button className="btn btn-soft btn-primary">Primary</button>
+            <button className="btn btn-soft btn-secondary">Secondary</button>
+            <button className="btn btn-soft btn-accent">Accent</button>
+            <button className="btn btn-soft btn-info">Info</button>
+            <button className="btn btn-soft btn-success">Success</button>
+            <button className="btn btn-soft btn-warning">Warning</button>
+            <button className="btn btn-soft btn-error">Error</button>
         </div>
     )
 }
