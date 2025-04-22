68f851cb20e455af3627b4300ad99a0a85d7c741
martinristow <martinristov084@gmail.com>
2025-04-22 23:50:26 +0200

    metadata

--- next-js/hello-world/src/app/about/page.tsx
+++ next-js/hello-world/src/app/about/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+    title: "About Martin",
+    description: "Ideee"
+}
+
 const About = () => {
     return (
         <div>About</div>
--- next-js/hello-world/src/app/products/[productId]/page.tsx
+++ next-js/hello-world/src/app/products/[productId]/page.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import {Metadata} from "next";
 
-const ProductsID = async ({params}: { params: Promise<{ productId: string }> }) => {
+type Props = {
+    params: {
+        productId: string;
+    }
+}
+
+// export const generateMetadata =  async ({params}: Props): Promise<Metadata> => {
+//
+//     const title = await new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(`Iphone ${title}`);
+//         }, 100);
+//     });
+//
+//     return {
+//         title: `Product ${params.productId}`
+//     }
+// }
+
+
+const ProductsID = async ({params}: Props) => {
     const {productId} = await params
     return (
         <div>Details about product {productId}</div>
