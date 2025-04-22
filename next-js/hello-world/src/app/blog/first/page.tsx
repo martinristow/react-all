e01547422b53e4aae3900ff8d5552ee330a05d3c
martinristow <martinristov084@gmail.com>
2025-04-22 17:44:53 +0200

    nested routes

---
+++ next-js/hello-world/src/app/blog/first/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const FirstBlog = () => {
+    return (
+        <div>FirstBlog</div>
+    )
+}
+export default FirstBlog
---
+++ next-js/hello-world/src/app/blog/second/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SecondBlog = () => {
+    return (
+        <div>SecondBlog</div>
+    )
+}
+export default SecondBlog
