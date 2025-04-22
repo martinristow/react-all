78e131763400e0e86d08234a654d170a775ee65c
martinristow <martinristov084@gmail.com>
2025-04-22 21:18:49 +0200

    file colocation

---
+++ next-js/hello-world/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BarChart = () => {
+    return (
+        <div>BarChart</div>
+    )
+}
+
+
+export default function Dashboard() {
+    return <h1>Dashboard</h1>
+}
