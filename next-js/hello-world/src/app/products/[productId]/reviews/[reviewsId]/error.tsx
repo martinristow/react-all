00894be82b4e318d93a8172119f483cd6f4d7476
martinristow <martinristov084@gmail.com>
2025-04-23 12:59:52 +0200

    Recovering from Errors

--- next-js/hello-world/src/app/products/[productId]/reviews/[reviewsId]/error.tsx
+++ next-js/hello-world/src/app/products/[productId]/reviews/[reviewsId]/error.tsx
@@ -1,5 +1,5 @@
 'use client'
 
-export default function errorBoundary({error} : {error: Error}){
-    return <div>{error.message}</div>
+export default function errorBoundary({error, reset} : {error: Error; reset: () => void}){
+    return <div>{error.message} <button className='cursor-pointer bg-gray-700' onClick={reset}>Try again</button></div>
 }
