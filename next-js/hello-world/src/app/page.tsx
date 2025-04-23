e7ce561ff1c97bbd5dcb66278c7b0e40fad6f884
martinristow <martinristov084@gmail.com>
2025-04-23 10:44:06 +0200

    active links

--- next-js/hello-world/src/app/page.tsx
+++ next-js/hello-world/src/app/page.tsx
@@ -1,11 +1,40 @@
+'use client'
+
 import Link from "next/link";
+import {usePathname} from "next/navigation";
+
+
+const navLinks = [
+    {name: "Home", href: "/"},
+    {name: "Blog", href: "/blog"},
+    {name: "Products", href: "/products"}
+]
 
 export const Home = () => {
+
+    const pathname = usePathname();
+
     return (
         <div>
             <h1>Hello</h1>
-            <Link href="/blog">Blog</Link>
-            <Link href="/products">Products</Link>
+            {
+                navLinks.map((link, index) => {
+                    const isActive = pathname.startsWith(link.href);
+                    return (
+                        <Link
+                            href={link.href}
+                            key={index}
+                            className={isActive ? "font-bold mr-4" : "text0blue-500 mr-4"}
+                        >
+                            {link.name}
+                        </Link>
+                    )
+                })
+            }
+
+
+            {/*<Link href="/blog">Blog</Link>*/}
+            {/*<Link href="/products">Products</Link>*/}
         </div>
     )
 }
