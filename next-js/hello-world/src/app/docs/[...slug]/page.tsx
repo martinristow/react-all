8989db32303976895a1f7baa64e8477db40c2427
martinristow <martinristov084@gmail.com>
2025-04-23 12:32:48 +0200

    catch all segments

--- next-js/hello-world/src/app/docs/[...slug]/page.tsx
+++ next-js/hello-world/src/app/docs/[...slug]/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Docs = async ({params}: {params: {slug:string[]}}) => {
+const Docs = async ({params}: { params: Promise<{ slug: string[] }> }) => {
+    const resolvedParams = await params;
 
-    if(params?.slug.length === 2) {
+    if (resolvedParams.slug.length === 2) {
         return (
-            <h1>Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}</h1>
+            <h1>Viewing docs for feature {resolvedParams.slug[0]} and concept {resolvedParams.slug[1]}</h1>
         )
-    } else if (params?.slug.length === 1) {
+    } else if (resolvedParams.slug.length === 1) {
         return (
-            <h1>Viewing docs for feature {params.slug[0]}</h1>
+            <h1>Viewing docs for feature {resolvedParams.slug[0]}</h1>
         )
     }
 
@@ -16,4 +17,5 @@ const Docs = async ({params}: {params: {slug:string[]}}) => {
         <div>Docs</div>
     )
 }
+
 export default Docs
