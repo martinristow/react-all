09e241779c056db74946f8dcaaeb089d28d4121e
martinristow <martinristov084@gmail.com>
2025-04-24 18:58:28 +0200

    handling delete request

--- next-js/route-handlers-demo/src/app/comments/[id]/route.ts
+++ next-js/route-handlers-demo/src/app/comments/[id]/route.ts
@@ -19,3 +19,13 @@ export async function PATCH(request: Request, {params}: { params: { id: string }
     return Response.json(comments[index]);
 
 }
+
+
+export async function DELETE(request: Request, {params}: { params: { id: string } }) {
+
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+    const deletedComment = comments[index]
+    comments.splice(index, 1);
+    return Response.json(deletedComment);
+
+}
