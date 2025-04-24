e147bea1399accc4ca3f1fe1d0c88b20b513e805
martinristow <martinristov084@gmail.com>
2025-04-24 18:41:36 +0200

    dynamic route handlers

---
+++ next-js/route-handlers-demo/src/app/comments/[id]/route.ts
@@ -0,0 +1,8 @@
+import {comments} from "@/app/comments/data";
+
+export async function GET(request: Request, {params}: { params: { id: string } }) {
+
+    const comment = comments.find((comment) => comment.id === parseInt(params.id));
+
+    return Response.json(comment);
+}
