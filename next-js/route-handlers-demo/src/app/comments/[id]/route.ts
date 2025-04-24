dbecf32dbe148ff5af1fbc15f2b4bf6108aa038a
martinristow <martinristov084@gmail.com>
2025-04-24 18:50:07 +0200

    handling patch request

--- next-js/route-handlers-demo/src/app/comments/[id]/route.ts
+++ next-js/route-handlers-demo/src/app/comments/[id]/route.ts
@@ -6,3 +6,16 @@ export async function GET(request: Request, {params}: { params: { id: string } }
 
     return Response.json(comment);
 }
+
+
+export async function PATCH(request: Request, {params}: { params: { id: string } }) {
+
+    const body = await request.json();
+    const { text } = body;
+
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+
+    comments[index].text = text;
+    return Response.json(comments[index]);
+
+}
