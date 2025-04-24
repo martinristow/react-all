3e771a572bb051aefc7c0d75c9db52a6d3df8205
martinristow <martinristov084@gmail.com>
2025-04-24 19:47:56 +0200

    url query parameters

--- next-js/route-handlers-demo/src/app/comments/route.ts
+++ next-js/route-handlers-demo/src/app/comments/route.ts
@@ -1,7 +1,16 @@
 import {comments} from "@/app/comments/data";
+import {NextRequest} from "next/server";
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    console.log(searchParams);
+    const query = searchParams.get("query");
+    console.log(query);
+    const filteredComments = query ?
+        comments.filter(comment => comment.text.includes(query)) :
+        comments;
+    return Response.json(filteredComments);
+
 }
 
 export async function POST(request: Request) {
