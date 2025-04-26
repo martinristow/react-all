8b90c699d80490ddae5d2a2e62812196da76fc33
martinristow <martinristov084@gmail.com>
2025-04-26 14:54:51 +0200

    dynamic rendering

--- next-js/rendering-demo/src/app/about/page.tsx
+++ next-js/rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,11 @@
-export default function AboutPage() {
+import {cookies} from "next/headers";
+
+export default async function AboutPage() {
+
+    const cookieStore = await cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
+
     console.log("About server component")
-    return <h1>About Page</h1>
+    return <h1>About Page {new Date().toLocaleString()}</h1>
 }
