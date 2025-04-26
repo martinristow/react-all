3576b194f56d73b5703eaaa518ddc2bf2e47999f
martinristow <martinristov084@gmail.com>
2025-04-26 16:58:41 +0200

    Context Providers

--- next-js/rendering-demo/src/app/client-route/page.tsx
+++ next-js/rendering-demo/src/app/client-route/page.tsx
@@ -2,12 +2,17 @@
 
 // import {serverSideFunction} from "@/utils/server-utils";
 
+import {useTheme} from "@/components/theme-provider";
+
 export default function ClientRoutePage(){
     console.log("Client route rendered")
     // const result = serverSideFunction();
+    const theme = useTheme();
     return (
         <div>
-            <h1>Server Route Page</h1>
+            <h1 style={{
+                color: theme.colors.secondary,
+            }}>Server Route Page</h1>
             {/*<p>{result}</p>*/}
         </div>
     )
