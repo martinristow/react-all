8a2bdcfd6ad4fd29c5dab0ac0043d1f7ec780b35
martinristow <martinristov084@gmail.com>
2025-04-26 15:11:52 +0200

    streaming

---
+++ next-js/rendering-demo/src/app/product-detail/page.tsx
@@ -0,0 +1,19 @@
+import {Suspense} from "react";
+
+import {Product} from "@/components/product";
+import {Review} from "@/components/reviews";
+
+export default function ProductDetailPage() {
+    return (
+        <div>
+            <h1>Product detail page</h1>
+            <Suspense fallback={<p>Loading product details...</p>}>
+                <Product/>
+            </Suspense>
+
+            <Suspense fallback={<p>Loading reviews...</p>}>
+                <Review/>
+            </Suspense>
+        </div>
+    )
+}
