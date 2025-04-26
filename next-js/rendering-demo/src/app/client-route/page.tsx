0607befaf3723601cc8e9bc8baa774135654b33b
martinristow <martinristov084@gmail.com>
2025-04-26 17:48:40 +0200

    Client-only Code

--- next-js/rendering-demo/src/app/client-route/page.tsx
+++ next-js/rendering-demo/src/app/client-route/page.tsx
@@ -3,17 +3,19 @@
 // import {serverSideFunction} from "@/utils/server-utils";
 
 import {useTheme} from "@/components/theme-provider";
+import {clientSideFunction} from "@/utils/client-utils";
 
 export default function ClientRoutePage(){
     console.log("Client route rendered")
     // const result = serverSideFunction();
+    const result = clientSideFunction()
     const theme = useTheme();
     return (
         <div>
             <h1 style={{
                 color: theme.colors.secondary,
             }}>Server Route Page</h1>
-            {/*<p>{result}</p>*/}
+            <p>{result}</p>
         </div>
     )
 }
--- next-js/rendering-demo/src/app/server-route/page.tsx
+++ next-js/rendering-demo/src/app/server-route/page.tsx
@@ -1,12 +1,15 @@
 import {serverSideFunction} from "@/utils/server-utils";
+import {clientSideFunction} from "@/utils/client-utils";
 
 export default function ServerRoutePage(){
     console.log("Server route rendered")
     const result = serverSideFunction();
+    const clientResult = clientSideFunction();
     return (
         <div>
             <h1>Server Route Page</h1>
             <p>{result}</p>
+            <p>{clientResult}</p>
         </div>
     )
 }
