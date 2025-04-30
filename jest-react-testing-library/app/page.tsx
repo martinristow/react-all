67a4df1ea4523c87bf6603058e9a8848b40ae897
martinristow <martinristov084@gmail.com>
2025-04-30 15:01:14 +0200

    implement TodoAndPhoto component

--- jest-react-testing-library/app/page.tsx
+++ jest-react-testing-library/app/page.tsx
@@ -1,11 +1,13 @@
 import Home from "../components/Home";
 import NotesForm from "@/components/NotesForm";
+import FetchTodos from "@/components/TodoAndPhotoList";
 
 export default function Home1() {
     return (
         <div className="size-full flex flex-col gap-8 items-center justify-center">
             <Home/>
             <NotesForm/>
+            <FetchTodos/>
         </div>
     );
 }
