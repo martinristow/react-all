50a78da5a6d9c63265f8f8fa6bf7594da6578c3d
martinristow <martinristov084@gmail.com>
2025-04-30 13:34:56 +0200

    implement notes form

--- jest-react-testing-library/app/page.tsx
+++ jest-react-testing-library/app/page.tsx
@@ -1,8 +1,11 @@
+import Home from "../components/Home";
+import NotesForm from "@/components/NotesForm";
 
-export default function Home() {
-  return (
-    <div className="size-full flex flex-col gap-8 items-center justify-center">
-      Home
-    </div>
-  );
+export default function Home1() {
+    return (
+        <div className="size-full flex flex-col gap-8 items-center justify-center">
+            <Home/>
+            <NotesForm/>
+        </div>
+    );
 }
