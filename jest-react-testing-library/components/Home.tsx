7d660f8f58c0044ebce7aa7ce8bd2b3e7ad90366
martinristow <martinristov084@gmail.com>
2025-04-30 13:00:22 +0200

    added paragraphs for testing

--- jest-react-testing-library/components/Home.tsx
+++ jest-react-testing-library/components/Home.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div><h1>
-            Home
-        </h1></div>
+        <div>
+            <h1>Home</h1>
+            <p data-testid="desc">This is my description</p>
+            <p data-testid="ide">This is my name</p>
+        </div>
     )
 }
 export default Home
