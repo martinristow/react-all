ac86d97b6454b8cfe6718ef18de6cbc8bc40a25d
martinristow <martinristov084@gmail.com>
2025-05-11 14:17:41 +0200

    implement Counter component

--- redux/redux1/src/App.jsx
+++ redux/redux1/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Counter from "./components/Counter.jsx";
 
 const App = () => {
     return (
-        <div>App</div>
+        <div><Counter/></div>
     )
 }
 export default App
